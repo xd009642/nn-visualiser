e7d114b147c3d362353fc8b2226bcb7e37e1d459
xd009642 <danielmckenna93@gmail.com>
2021-07-18 16:52:34 +0100

    Add ability to limit output depth, reverse arrows

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ pub struct Config {
     /// Save rendered output here
     #[structopt(short, long)]
     output: Option<PathBuf>,
+    /// Maximum depth to recurse into nested blocks
+    #[structopt(long)]
+    max_depth: Option<usize>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -28,6 +31,22 @@ impl Node {
         let name = PathBuf::from(op.name().expect("Op name not valid unicode"));
         let ty = op.op_type().expect("Op type not valid unicode");
         Self { name, ty }
+    }
+
+    fn from_operation_with_depth(op: &Operation, max_depth: usize) -> Self {
+        let mut ret = Self::from_operation(op);
+        ret.limit_depth(max_depth);
+        ret
+    }
+
+    fn limit_depth(&mut self, max_depth: usize) {
+        let depth = self.name.components().count();
+        for _ in max_depth..depth {
+            self.name.pop();
+        }
+        if max_depth < depth {
+            self.ty = "Block".to_string();
+        }
     }
 }
 
@@ -43,32 +62,45 @@ fn add_op_to_graph(
     output: (&Operation, Option<usize>),
     graph: &mut Graph<Node, Edge>,
     nodes: &mut HashMap<Node, NodeIndex>,
+    max_depth: Option<usize>,
 ) {
-    let in_node = Node::from_operation(&input.0);
+    let in_node = max_depth.as_ref().map_or_else(
+        || Node::from_operation(&input.0),
+        |index| Node::from_operation_with_depth(&input.0, *index),
+    );
     let in_idx = *nodes
         .entry(in_node.clone())
         .or_insert_with(|| graph.add_node(in_node.clone()));
-    let out_node = Node::from_operation(&output.0);
+    let out_node = max_depth.as_ref().map_or_else(
+        || Node::from_operation(&output.0),
+        |index| Node::from_operation_with_depth(&output.0, *index),
+    );
     let out_idx = *nodes
         .entry(out_node.clone())
         .or_insert_with(|| graph.add_node(out_node.clone()));
-    if graph.find_edge(in_idx, out_idx).is_none() {
+    if in_node != out_node && graph.find_edge(out_idx, in_idx).is_none() {
         let edge = Edge {
             dim: vec![],
             input_index: input.1,
             output_index: output.1,
         };
-        graph.add_edge(in_idx, out_idx, edge);
+        graph.add_edge(out_idx, in_idx, edge);
     }
 }
 
-fn generate_graph(nn_graph: &TfGraph) -> Graph<Node, Edge> {
+fn generate_graph(nn_graph: &TfGraph, max_depth: Option<usize>) -> Graph<Node, Edge> {
     let mut graph = Graph::<Node, Edge>::new();
     let mut nodes = HashMap::new();
     for op in nn_graph.operation_iter() {
         for i in 0..op.num_inputs() {
             let (input, idx) = op.input(i);
-            add_op_to_graph((&op, Some(i)), (&input, Some(idx)), &mut graph, &mut nodes);
+            add_op_to_graph(
+                (&op, Some(i)),
+                (&input, Some(idx)),
+                &mut graph,
+                &mut nodes,
+                max_depth.clone(),
+            );
         }
         for i in 0..op.num_outputs() {
             for (output, idx) in op.output_consumers(i).iter() {
@@ -77,14 +109,27 @@ fn generate_graph(nn_graph: &TfGraph) -> Graph<Node, Edge> {
                     (&op, Some(i)),
                     &mut graph,
                     &mut nodes,
+                    max_depth.clone(),
                 );
             }
         }
         for output in op.control_outputs().iter() {
-            add_op_to_graph((&output, None), (&op, None), &mut graph, &mut nodes);
+            add_op_to_graph(
+                (&output, None),
+                (&op, None),
+                &mut graph,
+                &mut nodes,
+                max_depth.clone(),
+            );
         }
         for input in op.control_inputs().iter() {
-            add_op_to_graph((&op, None), (&input, None), &mut graph, &mut nodes);
+            add_op_to_graph(
+                (&op, None),
+                (&input, None),
+                &mut graph,
+                &mut nodes,
+                max_depth.clone(),
+            );
         }
     }
     graph
@@ -97,7 +142,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut graph = TfGraph::new();
     graph.import_graph_def(&input, &ImportGraphDefOptions::new())?;
-    let graph = generate_graph(&graph);
+    let graph = generate_graph(&graph, config.max_depth);
     let dot = Dot::new(&graph);
 
     if let Some(o) = config.output {
