f3106521003af24fb36eb8b933abbe98bd51f40c
xd009642 <danielmckenna93@gmail.com>
2021-07-18 09:40:09 +0100

    Add control inputs and outputs
    Also make edge indexes optional as they don't see to exist for control
    operations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use petgraph::dot::Dot;
-use petgraph::graph::Graph;
+use petgraph::graph::{Graph, NodeIndex};
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
@@ -33,49 +33,57 @@ impl Node {
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Edge {
     dim: Vec<Option<usize>>,
-    input_index: usize,
-    output_index: usize,
+    input_index: Option<usize>,
+    output_index: Option<usize>,
+}
+
+fn add_op_to_graph(
+    input: (&Operation, Option<usize>),
+    output: (&Operation, Option<usize>),
+    graph: &mut Graph<Node, Edge>,
+    nodes: &mut HashMap<Node, NodeIndex>,
+) {
+    let in_node = Node::from_operation(&input.0);
+    let in_idx = *nodes
+        .entry(in_node.clone())
+        .or_insert_with(|| graph.add_node(in_node.clone()));
+    let out_node = Node::from_operation(&output.0);
+    let out_idx = *nodes
+        .entry(out_node.clone())
+        .or_insert_with(|| graph.add_node(out_node.clone()));
+    if graph.find_edge(in_idx, out_idx).is_none() {
+        let edge = Edge {
+            dim: vec![],
+            input_index: input.1,
+            output_index: output.1,
+        };
+        graph.add_edge(in_idx, out_idx, edge);
+    }
 }
 
 fn generate_graph(nn_graph: &TfGraph) -> Graph<Node, Edge> {
     let mut graph = Graph::<Node, Edge>::new();
     let mut nodes = HashMap::new();
     for op in nn_graph.operation_iter() {
-        let node = Node::from_operation(&op);
-        let node_idx = nodes
-            .entry(node.clone())
-            .or_insert_with(|| graph.add_node(node.clone()))
-            .clone();
         for i in 0..op.num_inputs() {
             let (input, idx) = op.input(i);
-            let in_node = Node::from_operation(&input);
-            let in_idx = nodes
-                .entry(in_node.clone())
-                .or_insert_with(|| graph.add_node(in_node.clone()));
-            let edge = Edge {
-                dim: vec![],
-                input_index: i,
-                output_index: idx,
-            };
-            graph.add_edge(*in_idx, node_idx, edge);
+            add_op_to_graph((&op, Some(i)), (&input, Some(idx)), &mut graph, &mut nodes);
         }
         for i in 0..op.num_outputs() {
             for (output, idx) in op.output_consumers(i).iter() {
-                let out_node = Node::from_operation(&output);
-                if nodes.contains_key(&out_node) {
-                    continue;
-                }
-                let out_idx = nodes
-                    .entry(out_node.clone())
-                    .or_insert_with(|| graph.add_node(out_node.clone()));
-
-                let edge = Edge {
-                    dim: vec![],
-                    input_index: *idx,
-                    output_index: i,
-                };
-                graph.add_edge(*out_idx, node_idx, edge);
+                add_op_to_graph(
+                    (&output, Some(*idx)),
+                    (&op, Some(i)),
+                    &mut graph,
+                    &mut nodes,
+                );
             }
+        }
+        for output in op.control_outputs().iter() {
+            add_op_to_graph((&output, None), (&op, None), &mut graph, &mut nodes);
+        }
+        for input in op.control_inputs().iter() {
+            add_op_to_graph((&op, None), (&input, None), &mut graph, &mut nodes);
         }
     }
     graph
@@ -85,7 +93,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::from_args();
 
     let input = fs::read(&config.input)?;
-    
+
     let mut graph = TfGraph::new();
     graph.import_graph_def(&input, &ImportGraphDefOptions::new())?;
     let graph = generate_graph(&graph);
